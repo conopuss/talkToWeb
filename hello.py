dc5b8de3052b8c8bdc38b17d25fc7db0ec50d3c4
irmak <irmak.sabuncu@yahoo.com.tr>
2025-02-13 15:19:17 +0300

    2. ders ödevi

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print('Hello World')
+"""
+Talktoweb, 1. hafta, 2. ders,
+veri tipleri ve input için alıştırma;
+"""
+
+x = int (input('bir sayı giriniz '))
+y = int (input('ikinci sayıyı giriniz'))
+z = x + y
+a = type(x)
+b = type(y)
+print(f'girdiğin sayıların x = {x} y = {y} toplamı: {z}')
+print("Ayrıca x ve y değişkenlerinin veri tipleri sırasıyla, x = {0}, y = {1} olarak yer alıyor ".format(a,b))
+
+
