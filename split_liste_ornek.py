499c13ce4492cc7d4ef0b7df316ffd51d1f3a5a3
irmak <irmak.sabuncu@yahoo.com.tr>
2025-02-19 21:47:21 +0300

    split, liste, lenght alıştırması

---
+++ split_liste_ornek.py
@@ -0,0 +1,7 @@
+x = """TFF Başkanı İbrahim Hacıosmanoğlu'nun da açıklamasıyla Galatasaray - Fenerbahçe arasında 
+oynanacak olan dev derbiyi yabancı hakemin yöneteceği kesinleşti. Şimdi tüm Türkiye, 'Hacıosmanoğlu'nun
+ 'dünya çapında' diye nitelediği hakemi merak ediyor. Derbiyi yönetmesi en muhtemel isimler ve favori
+  hakem de belli oldu."""
+
+list_x = x.split(' ')
+print(len(list_x))
